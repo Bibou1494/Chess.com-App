43548d5d1c8db26ed27536d0e4f9edc673111063
Bibou1494 <132447113+Bibou1494@users.noreply.github.com>
2025-04-15 18:48:24 +0200

    modified:   main.js modified:   package-lock.json modified:   package.json

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, dialog } = require('electron');
 const path = require('path');
 const template = require('./menubar');
 const dns = require('dns');
 const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
 
 let mainWindow;
 
 const menu = Menu.buildFromTemplate(template)
 Menu.setApplicationMenu(menu)
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
 
 function createWindow() {
     mainWindow = new BrowserWindow({
@@ -17,7 +21,7 @@ function createWindow() {
         frame: true,
         autoHideMenuBar: true,  // Automatically hide the menu bar
         webPreferences: {
-            nodeIntegration: false,
+            nodeIntegration: true,
         }
     });
 
@@ -45,25 +49,40 @@ app.whenReady().then(() => {
             createWindow();
         }
     });
-
-    // Check for updates
+    
     autoUpdater.checkForUpdatesAndNotify();
 });
 
 autoUpdater.on('update-available', () => {
     console.log('Update available!');
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Update Available',
+        message: 'An update is available and is being downloaded.',
+    });
 });
 
 autoUpdater.on('update-not-available', () => {
     console.log('No updates available.');
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'No Updates',
+        message: 'Your application is up-to-date.',
+    });
 });
 
 autoUpdater.on('error', (err) => {
     console.error('Error checking for updates:', err);
+    dialog.showErrorBox('Update Error', 'An error occurred while checking for updates. Please check the logs for more details.');
 });
 
 autoUpdater.on('update-downloaded', () => {
     console.log('Update downloaded. It will be installed on restart.');
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Update Ready',
+        message: 'Update downloaded. It will be installed on restart.',
+    });
 });
 
 app.on('window-all-closed', () => {
